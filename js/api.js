e9234dce12970b7b059051ba9e7c3b1781f54c6c
shenry45 <shawn45henry@gmail.com>
2019-03-30 16:43:35 -0700

    fix month generation for API call

--- js/api.js
+++ js/api.js
@@ -14,7 +14,6 @@ export async function getTickets(type, input) {
                 // save event details
                 events, eventDetails = data._embedded.events;
 
-                console.log(data._embedded.events);
                 return events;
             })
         .catch(error => console.log(error));
@@ -22,14 +21,14 @@ export async function getTickets(type, input) {
 
 const APIfuncCheck = (type, input) => {
     const proxy = 'https://cors-anywhere.herokuapp.com/';
-    const url = `https://app.ticketmaster.com/discovery/v2/events?apikey=${apikey}`;
+    const url = `http://app.ticketmaster.com/discovery/v2/events?apikey=${apikey}`;
     const params = `&startDateTime=${getDate()}&size=16&sort=date,asc&countryCode=US`;
 
     //check if search query or from genre search
     if (type === 'evnt') {
         return `${proxy}${url}&keyword=${input}${params}` 
     }  else {
-        return `${proxy}${url}${params}&genreId=${input}`;
+        return `${url}${params}&genreId=${input}`;
     }
 }
 
@@ -49,15 +48,21 @@ function getDate() {
         types.forEach(e => {
             //convert types var to JS date code
             let unit = eval(`nowDate.get${e}()`);
-            console.log(unit);
 
-            //(e === "Month") ? unit += 1 : null;
-            
-            (unit < 10) ? typesVal.push(`0${unit}`) : typesVal.push(unit);
+            if (unit < 10 && e === 'Month') {
+                // add 1 to the month number because count starts at 1 instead of 0
+                typesVal.push(`0${unit + 1}`)
+            } else if (unit < 10) {
+                // add 0 before number
+                typesVal.push(`0${unit}`)
+            } else {
+                typesVal.push(unit)
+            }
         });
 
         switch (type) {
             case 'month':
+                console.log(typesVal[0]);
                 return typesVal[0];
                 break;
             case 'date':
