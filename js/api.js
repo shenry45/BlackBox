1372af0df9e54e40b1344cf96fa17f2743967dcd
shenry45 <shawn45henry@gmail.com>
2019-03-30 09:32:33 -0700

    in progress of fixing json request format

--- js/api.js
+++ js/api.js
@@ -3,12 +3,15 @@ export let eventDetails;
 // put apikey in separate file and export variable as 'apikey'
 import { apikey } from '/js/apikey.js';
 
-export async function getTickets(type, key) {
+export async function getTickets(type, input) {
     let events;
-
-    await fetch(APIfuncCheck(type, key))
+    //
+    await fetch(APIfuncCheck(type, input))
         .then(response => response.json())
         .then(data => {
+
+            console.log(data);
+                // save event details
                 events, eventDetails = data._embedded.events;
 
                 console.log(data._embedded.events);
@@ -17,18 +20,16 @@ export async function getTickets(type, key) {
         .catch(error => console.log(error));
 }
 
-function APIfuncCheck(type, key) {
+const APIfuncCheck = (type, input) => {
+    const proxy = 'https://cors-anywhere.herokuapp.com/';
     const url = `https://app.ticketmaster.com/discovery/v2/events?apikey=${apikey}`;
     const params = `&startDateTime=${getDate()}&size=16&sort=date,asc&countryCode=US`;
 
-    console.log(url+params);
-
     //check if search query or from genre search
-    if (type === 'evnt'){
-        return `${url}&keyword=${key}${params}`;
-    } else {
-        console.log(key);
-        return `${url}${params}&genreId=${key}`;
+    if (type === 'evnt') {
+        return `${proxy}${url}&keyword=${input}${params}` 
+    }  else {
+        return `${proxy}${url}${params}&genreId=${input}`;
     }
 }
 
@@ -45,9 +46,10 @@ function getDate() {
         const typesVal = [];
 
         //convert each Date value to 2 digits, ie 1 -> 01
-        types.forEach(function(e) {
+        types.forEach(e => {
             //convert types var to JS date code
             let unit = eval(`nowDate.get${e}()`);
+            console.log(unit);
 
             //(e === "Month") ? unit += 1 : null;
             
@@ -76,5 +78,6 @@ function getDate() {
         };
     }
    
+    console.log(selDate)
     return selDate;
 }
